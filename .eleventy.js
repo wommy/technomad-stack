8d06ad9ada80a876a77a2f41291b420b9ef0388b
wommy <tom@technomad.media>
2019-11-11 12:53:12 -0500

    noClue - pushing

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
+// add syntaxHighlight
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function(eleventyConfig) {
 	// Copy the `static/` directory 
 	eleventyConfig.addPassthroughCopy("_static");
+
+	//add syntaxHighlight
+  eleventyConfig.addPlugin(syntaxHighlight);
 	
 	return {
 		passthroughFileCopy: true,
