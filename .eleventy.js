dba4a36c11cd9e8fd8afb2baa24a73a52192b05e
wommy <tom@technomad.media>
2021-03-04 21:48:39 -0500

    static/ passthru, inc ~> components; index-tms
    11ty.conf
      config ~> conf
      passthruCopy /static
      includes ~> components
      templateFormat +pug
    css output ~> 11ty/static/css
    index.html ~> index-tms.html

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
-module.exports = (config) => {
+module.exports = conf => {
+	conf.addPassthroughCopy("static")
 	return {
 		dir: {
-			input: "11ty"
+			input: "11ty",
+			includes: "_components",
 		},
-		templateFormats: [ "html","css","md" ],
+		templateFormats: [ "html","css","md","pug" ],
 		htmlTemplateEngine: "njk",
 	};
 };
