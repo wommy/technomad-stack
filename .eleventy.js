c50332645c9eddcf88b2499a40f9ed56007ecac3
wommy <tom@technomad.media>
2021-04-20 19:48:16 -0400

    _includes ~> _components

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ module.exports = config => {
 	config.addPassthroughCopy({ "static" : "/" })
 	return {
 		dir: {
-			input: "11ty"
+			input: "11ty",
+			includes: "_components",
 		},
 		htmlTemplateEngine: "njk",
 	};
