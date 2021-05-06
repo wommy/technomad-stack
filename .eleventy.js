4346254d8a21c7ca891d30490b702ed4870b5377
wommy <tom@technomad.media>
2021-05-06 14:19:45 -0400

    fixup: refactor

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = config => {
 	config.addPassthroughCopy({ "static": "/" })
+	config.addWatchTarget("static")
 	config.setDataDeepMerge(true)
 	config.addPlugin(require("@11ty/eleventy-navigation"))
+	config.setWatchThrottleWaitTime(250)
 	return {
 		dir: {
 			input: "11ty",
