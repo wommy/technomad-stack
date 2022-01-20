fd1d01c007a79d00b6bd3cc6e8a3aa1a99ab1741
wommy <tom@technomad.media>
2022-01-19 23:20:31 -0500

    11: passthru

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,8 @@
 const yaml = require("js-yaml")
 module.exports = config => {
-	config.addPassthroughCopy({ "static": "/" })
-	config.setDataDeepMerge(true)
+	config.addPassthroughCopy({ "static/*": "/" })
 	config.addPlugin(require("@11ty/eleventy-navigation"))
 	config.addDataExtension("yml", contents => yaml.load(contents))
-	// config.setWatchThrottleWaitTime(1000)
-	// config.addWatchTarget("static/")
 	return {
 		dir: {
 			input: "11ty",
